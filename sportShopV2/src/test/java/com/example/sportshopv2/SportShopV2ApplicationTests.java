6521a58034adc398de6f21de1225639e33e006a5
trancongluc <trancongluc9zx@gmail.com>
2024-10-08 23:52:11 +0700

    done base

---
+++ sportShopV2/src/test/java/com/example/sportshopv2/SportShopV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportshopv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportShopV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
