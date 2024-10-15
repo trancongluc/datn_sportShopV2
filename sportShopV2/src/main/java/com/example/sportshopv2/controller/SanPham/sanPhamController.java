a0ff6dd87e853cc98f951afd5db31e5ae69eb799
trancongluc <trancongluc9zx@gmail.com>
2024-10-15 23:05:38 +0700

    update giao dien san pham

--- sportShopV2/src/main/java/com/example/sportshopv2/controller/SanPham/sanPhamController.java
+++ sportShopV2/src/main/java/com/example/sportshopv2/controller/SanPham/sanPhamController.java
@@ -2,11 +2,17 @@ package com.example.sportshopv2.controller.SanPham;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("san-pham")
 public class sanPhamController {
-    @GetMapping("/san-pham")
-    public String theLoai() {
+    @GetMapping("")
+    public String sanPham() {
         return "SanPham/san-pham";
+    }
+    @GetMapping("/them-san-pham")
+    public String theLoai() {
+        return "SanPham/them-san-pham";
     }
 }
--- sportShopV2/src/main/resources/static/js/script.js
+++ sportShopV2/src/main/resources/static/js/script.js
@@ -59,8 +59,3 @@ function showContent(itemId) {
         contentSection.style.display = 'block';
     }
 }
-
-
-
-
-
