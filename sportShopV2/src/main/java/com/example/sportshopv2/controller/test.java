5c21bbc7881b316dca63f61033a1e0b9e8ad28ea
trancongluc <trancongluc9zx@gmail.com>
2024-10-17 17:01:22 +0700

    update lại all giao diện

--- sportShopV2/src/main/java/com/example/sportshopv2/controller/test.java
+++ sportShopV2/src/main/java/com/example/sportshopv2/controller/test.java
@@ -1,7 +1,6 @@
-package com.example.sportshopv2.Controller;
+package com.example.sportshopv2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -17,49 +16,31 @@ public class test {
     public String b() {
         return "KhachHang/khachhang";
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> NguyenTuanThuat
     @GetMapping("/c")
     public String c() {
         return "KhachHang/tao-khach-hang";
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
     @GetMapping("/d")
     public String d() {
         return "BanHangTaiQuay/BanHangTaiQuay";
     }
->>>>>>> long
-=======
-//<<<<<<< Updated upstream
-//=======
-    @GetMapping("/d")
-    public String d() {
-        return "NhanVien/QL-nhan-vien";
-    }
+
+
     @GetMapping("/e")
     public String e() {
         return "NhanVien/nhan-vien-add";
     }
-//>>>>>>> Stashed changes
->>>>>>> minh
 
-=======
->>>>>>> NguyenTuanThuat
-
-=======
     @GetMapping("/giam-gia")
-        public String GiamGia(){
-            return "PhieuGiamGia/giamGia";
+    public String GiamGia() {
+        return "PhieuGiamGia/giamGia";
     }
+
     @GetMapping("/add-giam-gia")
-    public String AddGiamGia(){
+    public String AddGiamGia() {
         return "PhieuGiamGia/add.html";
     }
->>>>>>> tan
+
 }
