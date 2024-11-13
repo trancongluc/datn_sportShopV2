a5b6d42d88a2dc89262132b242a5043112144479
trancongluc <trancongluc9zx@gmail.com>
2024-11-13 14:44:24 +0700

    update all

--- sportShopV2/src/main/java/com/example/sportshopv2/controller/MuaSamController.java
+++ sportShopV2/src/main/java/com/example/sportshopv2/controller/MuaSamController.java
@@ -2,8 +2,10 @@ package com.example.sportshopv2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("ban-hang")
 public class MuaSamController {
     @GetMapping("/mua-sam")
     public String MuaSam(){
