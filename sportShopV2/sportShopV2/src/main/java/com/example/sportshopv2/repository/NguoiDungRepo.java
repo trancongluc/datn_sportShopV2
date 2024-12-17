ebe48424fb76c06331dfb5a524e335a9a1d86b0a
trancongluc <trancongluc9zx@gmail.com>
2024-12-17 15:56:03 +0700

    fix

--- sportShopV2/sportShopV2/src/main/java/com/example/sportshopv2/repository/NguoiDungRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.sportshopv2.repository;
-
-import com.example.sportshopv2.model.NguoiDung;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NguoiDungRepo extends JpaRepository<NguoiDung, Integer> {
-}
--- sportShopV2/sportShopV2/src/main/java/com/example/sportshopv2/repository/PhieuGiamGiaResponsitory.java
+++
@@ -1,8 +0,0 @@
-package com.example.sportshopv2.repository;
-
-import com.example.sportshopv2.model.PhieuGiamGia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhieuGiamGiaResponsitory extends JpaRepository<PhieuGiamGia, Integer> {
-
-}
